4c2b8538739314a8e049e636efb6b1eb2509210a
enjuto <javier.sanzenjuto@gmail.com>
2023-09-03 21:39:50 +0200

    clean modules variables, states and config

--- environment/dev/variables.tf
+++ environment/dev/variables.tf
@@ -1,32 +1,44 @@
-variable "environments" {  
-  type        = list(string)
-  description = "Environment aviables to deploy services. This come from TF_VAR_example as a environment variable"
-  #default     = "dev"
+#variable "postgres_services" {
+#  type = map(object({
+#    name     = string
+#    #username = string
+#    #password = string
+#  }))
+#}
+#
+variable "ENVIRONMENT" {  
+  type        = string
+  description = "Environment fixed in the environment variable TF_VAR_ENVIRONMENT"
+}
+
+variable "PERIMETER_DEV" {  
+  type        = string
+  description = "Perimeter IP for production environment"
+}
+
+variable "PERIMETER_PRO" {  
+  type        = string
+  description = "Perimeter IP for development environment"
+}
+
+variable "PERIMETER" {  
+  type        = string
+  description = "Perimeter IP for environment variable TF_PERIMETER"
+}
+
+variable "domain" {  
+  type        = string
+  description = "Lab domain for communications and web exposition"
 }
 
 variable "services" {
   type = map(object({
-    name        = string
-    #user        = string
-    #password    = string
-    #description   = string    
-    #internal      = bool
-    #external      = bool
-    #proxied       = bool
+    status          = bool
+    name            = string
+    description     = string
+    dns_internal    = bool
+    dns_external    = bool
+    secrets         = bool
+    postgres        = bool    
   }))
-  description = "Services aviables in the cluster"
-}
-
-variable "postgres_users" {
-  type = map(object({
-    name        = string
-    #user        = string
-    #password    = string
-  }))
-  description = "postgres database users"
-}
-
-variable "ENVIRONMENT" {  
-  type        = string
-  description = "Environment fixed in the environment variable TF_VAR_ENVIRONMENT"
 }
--- modules/vault/variables.tf
+++ modules/vault/variables.tf
@@ -19,8 +19,39 @@ variable "serv" {
   }))
   description = "Services aviables in the cluster"
 }
+variable "ENVIRONMENT" {  
+  type        = string
+  description = "Environment fixed in the environment variable TF_VAR_ENVIRONMENT"
+}
 
-variable "env" {  
-  type        = list(string)
-  description = "Environment aviables to deploy services"
+variable "PERIMETER_DEV" {  
+  type        = string
+  description = "Perimeter IP for production environment"
+}
+
+variable "PERIMETER_PRO" {  
+  type        = string
+  description = "Perimeter IP for development environment"
+}
+
+variable "PERIMETER" {  
+  type        = string
+  description = "Perimeter IP for environment variable TF_PERIMETER"
+}
+
+variable "domain" {  
+  type        = string
+  description = "Lab domain for communications and web exposition"
+}
+
+variable "services" {
+  type = map(object({
+    status          = bool
+    name            = string
+    description     = string
+    dns_internal    = bool
+    dns_external    = bool
+    secrets         = bool
+    postgres        = bool    
+  }))
 }
